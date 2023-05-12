3d8511bffcc62dc775222d1e4516c0a3aa9505ee
Adriano Lengruber <85639595+Adriano-Lengruber@users.noreply.github.com>
2023-05-12 12:04:58 -0300

    update

--- Exercicios/Funcoes/aula_classes_e_objetos_IMC.js
+++ Exercicios/Funcoes/aula_classes_e_objetos_IMC.js
@@ -14,9 +14,28 @@ class Pessoa {
         return this.peso / (this.altura * this.altura);
     }
 
+    classificarIMC () {
+        const imc = this.calcularIMC();
+
+        if (imc < 18.5) {
+            return('Abaixo do Peso!');
+        } else if (imc >= 18.5 && imc < 25) {
+            return('Peso Normal!');
+        } else if (imc >= 25 && imc < 30) {
+            return('Acima do Peso!');
+        } else if (imc >= 30 && imc < 40) {
+            return('Obeso!');
+        } else {
+            return('Obesidade Grave!');
+        }
+    }
+
 }
 
 const adriano = new Pessoa('Adriano', 75, 1.69);
-const joao = new Pessoa('Joao', 80, 1.68);
 
-console.log(adriano.calcularIMC());
+console.log(`Meu nome é ${adriano.nome} e meu IMC é ${adriano.calcularIMC()}`);
+
+console.log(`De acordo com meu IMC:${adriano.calcularIMC()} estou classificado como: ${adriano.classificarIMC()}`);
+
+
